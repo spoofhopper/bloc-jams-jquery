4ac9e0f205a74b905911217928f550266b443c0c
Morgan Holland <morgan.holland@gmail.com>
2018-02-11 20:01:30 -0800

    added initial time bar movement

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -34,4 +34,14 @@
       player.playPause(previousSong);
     });
 
+  setInterval(() => {
+    if (player.playState !== 'playing') {
+      return;
+    }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control input').val(percent);
+  }, 1000);
+
 }
